767bb86c95da7578bc41df28c908ab5ff51e9e8c
ddiddi <dhruv.diddi+1@gmail.com>
2015-10-26 11:50:52 -0500

    Contact form with validation test v1

--- master/app.py
+++ master/app.py
@@ -1,6 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, BooleanField, TextField, PasswordField,SubmitField, TextAreaField, validators
 
 app = Flask(__name__)
+
+ 
+class ContactForm(Form):
+  name = TextField("Name")
+  email = TextField("Email")
+  subject = TextField("Subject")
+  message = TextAreaField("Message")
+  submit = SubmitField("Send")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        return 'Form Posted'
+
+    elif request.method == 'GET':
+           return render_template('contact.html', form = form)
+
+    
 @app.route('/')
 def home_page():
 	return render_template('home_page.html')
